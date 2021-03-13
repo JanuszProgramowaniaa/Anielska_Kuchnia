9c643eca9766b1b1147393630aac701f7c08ce76
Tomasz <Tomasz@poformacie.pl>
2021-03-13 21:37:49 +0100

    Template for project v2

---
+++ anielska-kuchnia/src/component/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Baner from './Baner'
+export default function About() {
+    return (
+        <div>
+            <Baner title="About"/>
+            
+        </div>
+    )
+}
---
+++ anielska-kuchnia/src/component/Main.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Baner from './Baner'
+export default function Main() {
+    return (
+        <div>
+         <Baner title="Main page"/>
+        </div>
+    )
+}
