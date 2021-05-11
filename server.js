ee6372b748b721b82210196404fc1126656dcfe5
Tomasz <tomke1410@gmail.com>
2021-05-11 16:34:21 +0200

    "deployment settings"

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path')
 
 require('dotenv').config();
 
@@ -25,7 +26,11 @@ app.use('/recipes',recipeRouter);
 
 
 if(process.env.NODE_ENV ==='production'){
-  app.use(express.static('../build'))
+  app.use(express.static('client/build'))
+
+  app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+  })
 }
 
 app.listen(port, () => {
